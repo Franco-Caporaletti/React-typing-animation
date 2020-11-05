f5bc5dcaa0e1e7f2df8dbc69f31218aed09c3823
Franco-Caporaletti <franco.caporaletti@outlook.es>
2020-11-05 03:23:40 -0300

    'typing'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Typical from 'react-typical';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://avatars2.githubusercontent.com/u/63564492?s=460&u=1f8d12bf53b8dd5e1daf8010a03a671dad5fc92d&v=4" className="App-logo" alt="logo" />    
+        <h1> Hi, i'm Franco Caporaletti</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm {' '}
+          <Typical
+            loop={Infinity} //Si cambias infinity por 1 solo reproduce el texto 1 vez.
+            wrapper="b"
+            steps={[
+              'full stack developer',
+              1000,
+              'creative',
+              1000,
+              'movie lover',
+              1000,
+              'drummer',
+              1000,
+              '24 years old',
+              1000
+            ]}
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
